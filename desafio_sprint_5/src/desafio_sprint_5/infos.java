8f97a571046371634eef8abdbe9423fb5bc9ce2b
DouglasKuhn <43782830+DouglasKuhn@users.noreply.github.com>
2020-01-13 16:51:19 -0300

    Initial commit

---
+++ desafio_sprint_5/src/desafio_sprint_5/infos.java
@@ -0,0 +1,30 @@
+package desafio_sprint_5;
+
+import java.util.Scanner;
+
+public class infos {
+	private Scanner sc;
+
+	public infos() {
+		this.sc = new Scanner(System.in);
+	}
+	
+	public pessoa pegaInfos( ) {
+		pessoa p = new pessoa();
+		
+		System.out.print("Digite o Código: ");
+		p.setCodigo(Integer.parseInt(sc.nextLine()));
+		
+		System.out.print("Digite o nome: ");
+		p.setNome(sc.nextLine());
+		
+		System.out.print("Digite o endereço: ");
+		p.setEndereco(sc.nextLine());
+		
+		System.out.print("Digite a idade: ");
+		p.setIdade(Integer.parseInt(sc.nextLine()));
+		
+		System.out.println();
+		return p;
+	}
+}
