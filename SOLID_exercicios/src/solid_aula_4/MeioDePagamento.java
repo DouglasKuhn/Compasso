066f93aaaa613cf21a5fbc55f8b4694e6a9043b2
DouglasKuhn <43782830+DouglasKuhn@users.noreply.github.com>
2020-01-21 16:41:18 -0300

    SOLID_ex_aula_4
    Exercicios SOLID aula 4 completo

---
+++ SOLID_exercicios/src/solid_aula_4/MeioDePagamento.java
@@ -0,0 +1,7 @@
+package solid_aula_4;
+
+public enum MeioDePagamento {
+
+    BOLETO,
+    CARTAO
+}
---
+++ SOLID_exercicios/src/solid_aula_4/ProcessadorDeBoletos.java
@@ -0,0 +1,16 @@
+package solid_aula_4;
+
+import java.util.List;
+
+public class ProcessadorDeBoletos {
+
+    public void processa(List<Boleto> boletos, Fatura fatura) {
+
+        for(Boleto boleto : boletos) {
+            Pagamento pagamento = new Pagamento(boleto.getValor(), MeioDePagamento.BOLETO);
+            fatura.adicionaPagamento(pagamento);
+
+        }
+
+    }
+}
